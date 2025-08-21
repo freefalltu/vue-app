28d46e42e82f9d02a3243e8b3e65bf9935603ed8
Eugene <freefall@gmail.com>
2025-08-21 04:10:58 +0300

    Анимация при удалении, создании и сортировки постов

--- src/components/PostList.vue
+++ src/components/PostList.vue
@@ -1,12 +1,14 @@
 <template>
     <div v-if="posts.length > 0">
         <h3>Список постов</h3>
-        <PostItem
-        v-for="post in posts"
-        :post="post"
-        :key="post.id"
-        @remove="$emit('remove', post)"
-        />
+        <TransitionGroup name="post-list">
+            <PostItem
+                v-for="post in posts"
+                :post="post"
+                :key="post.id"
+                @remove="$emit('remove', post)"
+            />
+        </TransitionGroup>
     </div>
     <h2 v-else class="empty">
         Список постов пуст
@@ -30,5 +32,25 @@ export default {
 <style scoped>
 .empty {
     color: red;
+}
+
+.post-list-item {
+    display: inline-block;
+    margin-right: 10px;
+}
+
+.post-list-enter-active,
+.post-list-leave-active {
+    transition: all 0.4s ease;
+}
+
+.post-list-enter-from,
+.post-list-leave-to {
+    opacity: 0;
+    transform: translateX(130px);
+}
+
+.post-list-move {
+    transition: transform 0.4s ease;
 }
 </style>
